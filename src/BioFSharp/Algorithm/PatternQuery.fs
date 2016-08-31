b694cc59e35f9a87d2abba02f53bdc381302c52b
kMutagene <kevin-schneider@mutagene.de>
2016-08-31 15:23:59 +0200

    Added KMP Algorithm

--- src/BioFSharp/Algorithm/PatternQuery.fs
+++ src/BioFSharp/Algorithm/PatternQuery.fs
@@ -68,3 +68,184 @@ module RabinKarp =
                 -1
         loop 0 initHs
 
+
+//Index out of range!
+    let rec SearchAll (pattern:array<'a>) (s:array<'a>) (startPos:int) (resultList : int list) =
+        if (startPos + pattern.Length) < s.Length then
+            let leMatch = SearchAt pattern s startPos
+            SearchAll pattern s leMatch (leMatch::resultList)
+        else
+            List.rev resultList
+
+
+
+
+module KnuthMorrisPratt = 
+
+    let createPrefixTable (pattern : array<'a>) =
+        let prefixTable = Array.zeroCreate pattern.Length
+    
+        let rec prefixSet startIndex matchCount =
+            
+            
+            let writeIndex = startIndex + matchCount
+            if writeIndex < pattern.Length then
+    
+    
+                if pattern.[writeIndex] = pattern.[matchCount] then
+                    
+    
+                    prefixTable.[writeIndex] <- matchCount
+    
+                    prefixSet startIndex (matchCount + 1)
+    
+    
+                else
+    
+                    prefixTable.[writeIndex] <- matchCount
+    
+                    prefixSet (writeIndex + 1) 0
+    
+        prefixSet 1 0
+    
+        if pattern.Length > 0 then prefixTable.[0] <- -1
+        prefixTable
+    
+    
+    
+    
+    //Algorithms to find all matches of a pattern in a text
+    
+    
+    
+    //without currying
+    //needs a precomputed prefixTable, done by the createPrefixTable function
+    
+    let findAllMatches (pattern : array<'a>) (text : array<'a>) (prefixTable : int [])=
+    
+        let rec matcher matchStart patternPosition resultList =
+    
+            if (matchStart + patternPosition + 1) <= text.Length then
+    
+                if pattern.[patternPosition] = text.[(matchStart + patternPosition)] then
+    
+                    if patternPosition = (pattern.Length - 1)  then
+    
+                        //printfn "Match found! starting at position %i" matchStart
+                        matcher (matchStart +  prefixTable.[(pattern.Length - 1)] + 1) 0 (matchStart::resultList)
+                    
+                    else
+                        matcher matchStart (patternPosition + 1) resultList
+    
+                else
+    
+                    if prefixTable.[patternPosition] > -1 then
+    
+                        matcher (matchStart + patternPosition - prefixTable.[patternPosition]) prefixTable.[patternPosition] resultList
+                    else
+                        
+                         
+                        matcher (matchStart + 1) 0 resultList
+            else
+                //printfn "Matches found starting on the following indices of the text:" 
+                List.rev resultList
+    
+        matcher 0 0 []    
+    
+    
+    let initFindAllMatches (pattern : array<'a>) = 
+        //makes sure that the pattern will only be used with its respective prefixTable
+        //initializing: let yourFunctionName = initFindAllMatches yourPattern 
+        //!! dont use the name of any of the already existing functions as the name of "yourFunction"!! 
+        //to find matches: yourFunctionName yourText
+    
+    
+        let prefixTable = createPrefixTable pattern
+    
+        let findAllMatches (pattern : array<'a>) (prefixTable : int []) (text : array<'a>)=
+    
+        //matchStart: start of the current match in the text.
+        //patternPosition: index of the current character in the pattern
+    
+            let rec matcher matchStart patternPosition resultList =
+    
+                if (matchStart + patternPosition + 1) <= text.Length then
+    
+                    if pattern.[patternPosition] = text.[(matchStart + patternPosition)] then
+    
+                        if patternPosition = (pattern.Length - 1)  then
+    
+                            //printfn "Match found! starting at position %i" matchStart
+                            matcher (matchStart +  prefixTable.[(pattern.Length - 1)] + 1) 0 (matchStart::resultList)
+    
+                        else
+                            matcher matchStart (patternPosition + 1) resultList
+    
+                    else
+    
+                    //printfn "5"
+    
+                        if prefixTable.[patternPosition] > -1 then
+    
+                            matcher (matchStart + patternPosition - prefixTable.[patternPosition]) prefixTable.[patternPosition] resultList
+    
+                        else
+    
+                            matcher (matchStart + 1) 0 resultList
+                else
+                    //printfn "Matches found starting on the following indices of the text:" 
+                    List.rev resultList
+    
+            matcher 0 0 [] 
+    
+        findAllMatches pattern prefixTable
+    
+    
+    
+    //first Match finders
+    //Algorithms for finding the first match of a pattern in a text. Terminates after the first match is found  
+    let findFirstMatch (prefixTable : int []) (pattern : array<'a>) (text : array<'a>) =
+    
+        let rec matcher matchStart patternPosition =
+    
+            if (matchStart + patternPosition + 1) <= text.Length then
+    
+                if pattern.[patternPosition] = text.[(matchStart + patternPosition)] then
+    
+                    if patternPosition = (pattern.Length - 1)  then
+    
+                        //printfn "Match found! starting at position %i" matchStart
+                        matchStart
+                    
+                    else
+                        matcher matchStart (patternPosition + 1) 
+    
+                else
+    
+                    if prefixTable.[patternPosition] > -1 then
+    
+                        matcher (matchStart + patternPosition - prefixTable.[patternPosition]) prefixTable.[patternPosition] 
+                    else
+                        
+                         
+                        matcher (matchStart + 1) 0 
+            else
+                //printfn "no matches found"
+                -1    
+                
+    
+        matcher 0 0 
+    
+    
+    
+    
+    let initFindFirstMatch (pattern : array<'a>) = 
+        //makes sure that the pattern will only be used with its respective prefixTable
+        //initializing: let yourFunctionName = initFindFirstMatch yourPattern 
+        //!! dont use the name of any of the already existing functions as the name of "yourFunction"!! 
+        //to find matches: yourFunctionName yourText
+    
+    
+        let prefixTable = createPrefixTable pattern
+        findFirstMatch  prefixTable pattern
+
---
+++ src/BioFSharp/Algorithm/Script1.fsx
@@ -0,0 +1,19 @@
+﻿#light
+#time
+#r @"C:\Users\Kevin-Laptop\Documents\GitHub\BioFSharp\bin\BioFSharp.dll"
+#r @"C:\Users\Kevin-Laptop\Documents\GitHub\BioFSharp\bin\BioFSharp.IO.dll"
+
+
+open BioFSharp
+open BioFSharp.IO
+open BioFSharp.Algorithm
+
+
+let pattern = "lol" |> Array.ofSeq
+let text = "ololllllllllllllllllllllllllllllllllllllllolllllllllllllllllllllllllllllolollllllo" |> Array.ofSeq
+
+let rabinKarpSearch = BioFSharp.Algorithm.RabinKarp.SearchAll
+let kMpSearch = BioFSharp.Algorithm.KnuthMorrisPratt.initFindAllMatches pattern
+
+rabinKarpSearch pattern text 0 []
+kMpSearch text
