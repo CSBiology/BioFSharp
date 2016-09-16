052564c1c961569367d710dacc32ef3fd6413945
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2016-09-16 16:31:53 +0200

    Add peak representation

---
+++ src/BioFSharp.Mz/Peak.fs
@@ -0,0 +1,16 @@
+﻿namespace BioFSharp.Mz
+
+
+type IPeak =    
+    abstract member Mz        : float
+    abstract member Intensity : float   
+
+
+[<Struct>]
+type Peak(mz:float,intensity:float) = 
+        member this.Mz = mz
+        member this.Intensity = intensity
+        
+        interface IPeak with
+            member this.Mz = mz
+            member this.Intensity = intensity
---
+++ src/BioFSharp.Mz/PeakArray.fs
@@ -0,0 +1,24 @@
+﻿namespace BioFSharp.Mz
+
+type PeakArray<[<EqualityConditionalOn; ComparisonConditionalOn >]'a when 'a :> IPeak> = array<'a>
+
+module PeakArray =
+
+    let map f pkarr : PeakArray<_> = 
+        Array.map f pkarr
+
+    let zipMzInt (mz:array<float>) (intensity:array<float>) : PeakArray<_> = 
+        Array.map2 (fun m i -> Peak(m,i)) mz intensity
+
+    let unzipMzInt (pkarr : PeakArray<_>) = 
+        let n = pkarr.Length
+        let mz     = Array.zeroCreate n
+        let intens = Array.zeroCreate n
+        for i=0 to n do
+            mz.[i]     <- pkarr.[i]
+            intens.[i] <- pkarr.[i]
+        mz,intens
+    
+        
+
+         
