bf43694df8c9703e33fc1be955236c8233bf17b7
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2017-04-24 19:41:50 +0200

    amino acid properties

--- docs/content/BioSequence.fsx
+++ docs/content/BioSequence.fsx
@@ -62,5 +62,28 @@ AminoAcids.Cys
 
 
 
+open AminoAcidSymbols
+
+let aminoPropertyBy defaultValue (str:string) =
+    let arr = 
+        str.Split(' ')
+        |> Array.map (fun v -> 
+            match parseChar v.[0] with
+            | _,Some ac -> int ac - 65, float v.[2..] 
+            | _ -> failwith "Error in AminoAcid code at given property" )
+    
+    // TODO: validation
+
+    let av = Array.create 25 defaultValue
+    for a in arr do 
+        av.[fst a] <- snd a
+    
+    (fun  (amino:AminoAcidSymbol) -> av.[int amino - 65])
+        
+let getHydrophobicityIndex = aminoPropertyBy nan "A:0.61 R:0.60 N:0.06 D:0.46 C:1.07 Q:0. E:0.47 G:0.07 H:0.61 I:2.22 L:1.53 K:1.15 M:1.18 F:2.02 P:1.95 S:0.05 T:0.05 W:2.65 Y:1.88 V:1.32"
+
+getHydrophobicityIndex AminoAcidSymbol.Asn
 
 
+int AminoAcidSymbol.Asn
+26 + 65 |> aminoAcidSymbol
--- src/BioFSharp/AminoAcidSymbols.fs
+++ src/BioFSharp/AminoAcidSymbols.fs
@@ -72,7 +72,7 @@ module AminoAcidSymbols =
         /// *  *Termination
         static member Ter = AminoAcidSymbol (byte '*')
 
-        //static member inline op_Explicit (value) = AminoAcidSymbol(byte value)
+        static member op_Explicit (value) = AminoAcidSymbol(byte value)
         static member op_Explicit (value:AminoAcidSymbol) : byte = value.Value
         static member op_Explicit (value:AminoAcidSymbol) : int = int value.Value
 
@@ -175,8 +175,8 @@ module AminoAcidSymbols =
                     | _ -> failwithf "Not an amino acid symbol"
                 
                 name this
-
-
+    // Conversion operators
+    let aminoAcidSymbol (a:int) = AminoAcidSymbol.op_Explicit(a)
 
     let inline parseChar (c:char) =
         match System.Char.ToUpper c with                                    
