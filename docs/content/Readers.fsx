45c509d98daad25710e202e5b86c469b6469c93e
Heinrich Lukas Weil <hl.weil@t-online.de>
2017-06-30 16:43:07 +0200

    -Added IO documentation tab with navigation pane

---
+++ docs/content/Readers.fsx
@@ -0,0 +1,48 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+
+(**
+Using Bio Format Readers
+==============================
+BioFSharp contains a set of readers for different biology-associated file formats. This documentation aims to give an introduction for them.
+
+<a name="FastQ"></a>
+FastQ
+------------------------------
+This documentation is not yet here. Hopefully it will be soon =)
+
+
+*)
+
+let a = 42
+printfn "hello world: %i " a
+
+
+
+(**
+
+<a name="FastA"></a>
+FastA
+------------------------------
+This documentation is not yet here. Hopefully it will be soon =)
+
+*)
+
+
+(**
+
+
+
+
+
+<a name="GFF3"></a>
+GFF3
+------------------------------
+Just put the stuff below here fenni&bella
+
+
+
+*)
