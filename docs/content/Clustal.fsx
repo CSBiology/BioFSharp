397538f315647b9338d2b71c92a88843c93ba0ca
HLWeil <hl.weil@t-online.de>
2017-09-11 22:36:41 +0200

    transfer reader tutorials

---
+++ docs/content/Clustal.fsx
@@ -0,0 +1,10 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+#r "../../bin/BioFSharp.dll"
+#r "../../bin/BioFSharp.IO.dll"
+#r "../../bin/FSharp.Care.dll"
+#r "../../bin/FSharp.Care.IO.dll"
+
+(** Work in progress *)
