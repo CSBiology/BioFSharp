b9880e0b1ce81c6954194766ba12e734cf77fa39
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2017-09-26 12:49:52 +0200

    Docs BioItem

--- docs/content/BioItem.fsx
+++ docs/content/BioItem.fsx
@@ -1,3 +1,31 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+#r "BioFSharp.dll"
+#r "BioFSharp.IO.dll"
 (**
-Work in progress...
+BioItem
+=========
+
+BioFSharp includes an efficient and type save way to work with biological items like amino acids and nucleotides. 
 *)
+
+open BioFSharp
+
+
+/// Returns the full name of the given amino acid 
+BioItem.name AminoAcids.Arg
+/// Returns the full name of the given nucleotide
+BioItem.name Nucleotides.A
+
+/// Returns the one-letter code 
+BioItem.symbol AminoAcids.Arg
+
+// Chemical formula
+BioItem.formula AminoAcids.Arg
+
+
+
+AminoAcids.isCharged AminoAcids.Arg
+
--- docs/content/Formula.fsx
+++ docs/content/Formula.fsx
@@ -2,6 +2,8 @@
 // This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../bin"
+#r "BioFSharp.dll"
+open BioFSharp
 
 (**
 Formula
@@ -10,8 +12,7 @@ Formula
 work in progress... 
 
 *)
-#r "BioFSharp.dll"
-open BioFSharp
+
 
 
 let CO2 = Formula.parseFormulaString "CO2"
