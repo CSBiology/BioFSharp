8cd3bb31a81cc07544422f082b93b133f2a23176
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2018-01-31 10:00:16 +0100

    Fixed Obo parser: unexpected end of file

--- docs/content/Obo.fsx
+++ docs/content/Obo.fsx
@@ -1,1 +1,19 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+#r "../../bin/BioFSharp.dll"
+#r "../../bin/BioFSharp.IO.dll"
+#r "../../bin/FSharp.Care.dll"
+#r "../../bin/FSharp.Care.IO.dll"
 (** Work in progress *)
+
+open FSharp.Care.IO
+open BioFSharp.IO
+
+let test = 
+    Seq.fromFile "Psi-MS.txt"
+    |> Obo.parseOboTerms
+    |> Seq.toArray
+
+test.[test.Length-1]
