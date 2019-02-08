6ba55166336bd2da332ef70e2b15ece2bf5be914
Kevin Schneider <kevin-schneider@mutagene.de>
2019-02-08 13:10:39 +0100

    Bump version to 0.013 - introduction of BioTools

--- src/BioFSharp.IO/AssemblyInfo.fs
+++ src/BioFSharp.IO/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("BioFSharp.IO")>]
 [<assembly: AssemblyProductAttribute("BioFSharp")>]
 [<assembly: AssemblyDescriptionAttribute("An open source bioinformatics toolbox written in F#. <https://csbiology.github.io/BioFSharp/>")>]
-[<assembly: AssemblyVersionAttribute("0.0.12")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.12")>]
+[<assembly: AssemblyVersionAttribute("0.0.13")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.13")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "BioFSharp.IO"
     let [<Literal>] AssemblyProduct = "BioFSharp"
     let [<Literal>] AssemblyDescription = "An open source bioinformatics toolbox written in F#. <https://csbiology.github.io/BioFSharp/>"
-    let [<Literal>] AssemblyVersion = "0.0.12"
-    let [<Literal>] AssemblyFileVersion = "0.0.12"
+    let [<Literal>] AssemblyVersion = "0.0.13"
+    let [<Literal>] AssemblyFileVersion = "0.0.13"
     let [<Literal>] AssemblyConfiguration = "Release"
