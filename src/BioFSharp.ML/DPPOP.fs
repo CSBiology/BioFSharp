24943fe83558bfa04a26e4ce3551a7551ac812a3
Kevin Schneider <kevin-schneider@mutagene.de>
2019-02-20 14:29:31 +0100

    Add digestion efficiency calculation to dppop classification Refactor existing functions and supply generic counterparts

--- src/BioFSharp.ML/DPPOP.fs
+++ src/BioFSharp.ML/DPPOP.fs
@@ -6,19 +6,25 @@ module DPPOP =
     open FSharpAux.IO
     open BioFSharp
     open BioFSharp.IO
+    open BioFSharp.Digestion
   
     ///
     module Classification =
 
-        ///
-        let digestTryptic (aminoAcidSeq:BioArray.BioArray<AminoAcids.AminoAcid>) =
+        ///Tryptic digestion of an amino acid sequence with the ability to control the maximal amount of misscleavages and filtering of a minimal peptide length
+        let digestTrypticWith (maxMissCleavages:int) (minPeptideLength:int) (aminoAcidSeq:BioArray.BioArray<AminoAcids.AminoAcid>) =
             Digestion.BioArray.digest (Digestion.Table.getProteaseBy "Trypsin") 0 aminoAcidSeq
-            |> Digestion.BioArray.concernMissCleavages 0 3
-            |> Seq.map (fun p -> p.PepSequence |> List.toArray) // TODO not |> List.toArray
-            |> Seq.filter (fun p -> p.Length > 6)
+            |> Digestion.BioArray.concernMissCleavages 0 maxMissCleavages
+            |> Seq.map (fun p -> p.PepSequence |> List.toArray)
+            |> Seq.filter (fun p -> p.Length > minPeptideLength)
+
+        ///Tryptic digestion of an amino acid sequence with the settings used for the dppop web API
+        let digestTryptic (aminoAcidSeq:BioArray.BioArray<AminoAcids.AminoAcid>) =
+            aminoAcidSeq
+            |> digestTrypticWith 3 6 
 
         ///
-        let getDistinctPeptidesFromFasta (fa:seq<FastA.FastaItem<BioArray.BioArray<AminoAcids.AminoAcid>>>)= 
+        let getDistinctTrypticPeptidesFromFasta (fa:seq<FastA.FastaItem<BioArray.BioArray<AminoAcids.AminoAcid>>>)= 
             //fileDir + "Chlamy_Cp.fastA"
             fa
             |> Seq.map (fun fi -> fi.Sequence |> Array.filter (not << AminoAcids.isTerminator))
@@ -35,6 +41,29 @@ module DPPOP =
             |> Seq.collect digestTryptic
             |> Seq.map BioArray.toString
             |> Set.ofSeq
+
+
+        let getDifestionEfficiency (protId) (sequence:BioArray.BioArray<AminoAcids.AminoAcid>) =
+            let cleavageScore = sequence |> Array.map AminoAcidSymbols.aminoAcidSymbol |> Digestion.CleavageScore.calculateCleavageScore
+        
+            //TODO: digestion hast changed from 1 based index to 0 based index, identify the numbers to change
+            let getStart index = if index < 2 then 0. else cleavageScore.[index-1]
+            let getEnd index = if index >= cleavageScore.Length  then 0. else cleavageScore.[index]
+
+
+            let calc (p:DigestedPeptide<int>) =
+                if p.MissCleavages < 1 then
+                    (protId,p.PepSequence |> Seq.map AminoAcidSymbols.aminoAcidSymbol |> Seq.toArray ),(getStart p.CleavageStart,0.,getEnd p.CleavageEnd)
+                else
+                    let inter' = p.MissCleavages - 1 |> float
+                    let s = getStart p.CleavageStart                
+                    let e = getEnd p.CleavageEnd
+                    // let inter' = inter - s - e
+                    (protId,p.PepSequence |> Seq.map AminoAcidSymbols.aminoAcidSymbol |> Seq.toArray),(s,inter',e)
+
+            Digestion.BioArray.digest (Digestion.Table.getProteaseBy "Trypsin") 0 sequence
+            |> Digestion.BioArray.concernMissCleavages 0 3
+            |> Seq.map calc
 
     ///
     module Prediction =
