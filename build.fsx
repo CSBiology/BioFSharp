d6971244c90b1c80467b3b0837b0f3c1c136502d
David Zimmer <davidzimmer91@gmail.com>
2019-04-26 12:38:30 +0200

    fix build target "Nuget"

--- build.fsx
+++ build.fsx
@@ -291,6 +291,7 @@ Target.create "RunTests" (fun _ ->
 Target.create "NuGet" (fun _ ->
     Paket.pack(fun p ->
         { p with
+            ToolPath=".paket/paket.exe"
             OutputPath = "bin"
             Version = release.NugetVersion
             ReleaseNotes = String.toLines release.Notes})
