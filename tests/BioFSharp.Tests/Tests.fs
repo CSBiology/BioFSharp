0de69196c1e6078e6fbb86f8b64d65371b207fb2
ZimmerD <davidzimmer91@gmail.com>
2019-07-31 17:57:38 +0200

    rename .fs in unit testing

--- tests/BioFSharp.Tests/Tests.fs
+++ tests/BioFSharp.Tests/Tests.fs
@@ -4,43 +4,24 @@ open Expecto
 open FsCheck
 open GeneratorsCode
 
-module Core =
-
-    open BioFSharp.Formula
+module Tests =
+    let config10k = { FsCheckConfig.defaultConfig with maxTest = 10000}
+    // bug somewhere:  registering arbitrary generators causes Expecto VS test adapter not to work
+    //let config10k = { FsCheckConfig.defaultConfig with maxTest = 10000; arbitrary = [typeof<Generators>] }
+    let configReplay = { FsCheckConfig.defaultConfig with maxTest = 10000 ; replay = Some <| (1940624926, 296296394) }
 
     [<Tests>]
-    let testFormula =
+    let testSimpleTests =
 
-        testList "BioFSharp.Formula" [
+        testList "DomainTypes.Tag" [
+            testCase "equality" <| fun () ->
+                let result = 42
+                Expect.isTrue (result = 42) "Expected True"
 
-            testCase "test_toString" <| fun () ->
-                let res = "H2O"  |> parseFormulaString |> toString
-                Expect.isTrue (res = "H2.00 O1.00") "Expected True"
-
-            testCase "test_add" <| fun () ->
-                let f1  = "H2"  |> parseFormulaString 
-                let f2  = "O"   |> parseFormulaString 
-                let res = add f1 f2 |> toString 
-                Expect.isTrue (res = "H2.00 O1.00") "Expected True"
-            
-            testCase "test_substract" <| fun () ->
-                let f1  = "H2O"  |> parseFormulaString 
-                let f2  = "H4O2" |> parseFormulaString 
-                let res = substract f1 f2 |> toString 
-                Expect.isTrue (res = "H2.00 O1.00") "Expected True"
-
-            testCase "test_substract_neg" <| fun () ->
-                let f1  = "H2O"   |> parseFormulaString 
-                let f2  = emptyFormula 
-                let res = substract f1 f2 |> toString 
-                Expect.isTrue (res = "H-2.00 O-1.00") "Expected True"
-
-            testCase "test_substract_Zero" <| fun () ->
-                let f1  = "H2O" |> parseFormulaString 
-                let f2  = "H2O" |> parseFormulaString 
-                let res = substract f1 f2 |> toString 
-                Expect.isTrue (res = "H0.00 O0.00") "Expected True"
-
+            testPropertyWithConfig config10k "whitespace" <|
+                fun  () ->
+                    Prop.forAll (Arb.fromGen <| whitespaceString())
+                        (fun (x : string) -> 
+                            x = x)
         ]
 
-        
