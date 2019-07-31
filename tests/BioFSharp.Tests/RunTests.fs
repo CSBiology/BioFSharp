12111240ddfe44adddd2979636c3b0310874e342
ZimmerD <davidzimmer91@gmail.com>
2019-07-31 17:55:12 +0200

    add Unit testing Core.Formula

--- tests/BioFSharp.Tests/RunTests.fs
+++ tests/BioFSharp.Tests/RunTests.fs
@@ -8,8 +8,8 @@ module RunTests =
     let main args =
 
         Tests.runTestsWithArgs defaultConfig args Tests.testSimpleTests |> ignore
-
         //BioFSharp core tests
+        Tests.runTestsWithArgs defaultConfig args Core.testFormula |> ignore
         //Run BioArrayTests
         Tests.runTestsWithArgs defaultConfig args BioArray.parsingTests  |> ignore
         0
