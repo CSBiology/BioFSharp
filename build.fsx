442708ed00d1c69b0bfdd5edb27dc78dfd66cb62
kMutagene <kevin-schneider@mutagene.de>
2020-02-19 11:37:54 +0100

    Fix excluded paths for dotnet configuration again

--- build.fsx
+++ build.fsx
@@ -259,11 +259,14 @@ Target.create "CopyBinariesMono" (fun _ ->
 
 Target.create "CopyBinariesDotnet" (fun _ ->
     printfn "excluding the biodb project for dotnet builds"
-    printfn "paths to copy : %A" (!! "src/**/*.??proj"-- "src/**/*.shproj" -- "src/BioFSharp.BioDb.fsproj"-- "src/BioFSharp.Parallel.fsproj"-- "src/BioFSharp.ImgP.fsproj" -- "src/BioFSharp.Vis.fsproj")
+    printfn "paths to copy : %A" (!! "src/**/*.??proj"-- "src/**/*.shproj" -- "src/BioFSharp.BioDb.fsproj" -- "src/BioFSharp.Parallel.fsproj" -- "src/BioFSharp.ImgP.fsproj" -- "src/BioFSharp.Vis.fsproj")
     let targets = 
         !! "src/**/*.??proj"
         -- "src/BioFSharp.BioDB/BioFSharp.BioDB.fsproj"
         -- "src/**/*.shproj"
+        -- "src/BioFSharp.Parallel.fsproj" 
+        -- "src/BioFSharp.ImgP.fsproj" 
+        -- "src/BioFSharp.Vis.fsproj"
         |>  Seq.map (fun f -> ((Path.getDirectory f) </> "bin" </> "DotnetCore", "bin" </> (Path.GetFileNameWithoutExtension f)))
     for i in targets do printfn "%A" i
     targets
