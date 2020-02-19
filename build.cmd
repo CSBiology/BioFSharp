ef0faefdf73fffdd8f9eadc5bcc9e4f5f1855705
kMutagene <kevin-schneider@mutagene.de>
2020-02-19 10:08:43 +0100

    Try CI fix #5

--- build.cmd
+++ build.cmd
@@ -3,4 +3,5 @@ cls
 
 dotnet tool restore
 dotnet paket restore
+dotnet restore build.proj
 dotnet fake build %*
--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ set -o pipefail
 
 dotnet tool restore
 dotnet paket restore
+dotnet restore build.proj
 dotnet fake build "$@"
