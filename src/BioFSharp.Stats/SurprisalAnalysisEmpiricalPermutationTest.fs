8848d0c1b5fa634e8add82a4d49e3a6c5d9af084
Lukas Weil <hl.weil@t-online.de>
2020-02-23 13:40:08 +0100

    fix Sailent.compute negativeBinSum
    Negative bins should not be assigned positive binSums, making the abs function erroneous
    
    Co-Authored-By: Kevin Schneider <kevin-schneider@mutagene.de>

--- src/BioFSharp.Stats/SurprisalAnalysisEmpiricalPermutationTest.fs
+++ src/BioFSharp.Stats/SurprisalAnalysisEmpiricalPermutationTest.fs
@@ -125,7 +125,7 @@ module Sailent =
             groups
             |> Array.map (fun (termName,tmp) ->   
                 let tmp = tmp |> Array.filter (fun ann -> ann.Item < 0.)
-                termName,tmp.Length,tmp |> Array.sumBy (fun x -> abs x.Item))
+                termName,tmp.Length,tmp |> Array.sumBy (fun x -> x.Item))
             |> Array.filter (fun (termName,binSize,weightSum) -> binSize>0)
 
         let absoluteBinsizes = absoluteTestTargets |> Array.map (fun (_,binSize,_) -> binSize) |> Array.distinct
