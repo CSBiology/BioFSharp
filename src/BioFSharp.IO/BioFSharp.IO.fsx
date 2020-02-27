130e1c63264989978e54f114dbd04b6dfb9458d3
kMutagene <kevin-schneider@mutagene.de>
2020-02-27 14:26:09 +0100

    Add load script for pretty printers to BioFSharp.IO

--- src/BioFSharp.IO/BioFSharp.IO.fsx
+++ src/BioFSharp.IO/BioFSharp.IO.fsx
@@ -1,8 +1,26 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿#nowarn "211"
+//Adapted from https://github.com/fslaborg/Deedle/blob/master/src/Deedle/Deedle.fsx
 
-//#load "Library1.fs"
-//open BioFSharp.IO
+// Standard NuGet or Paket location
+#I "."
+#I "lib/net45"
 
-// Define your library scripting code here
+// Try various folders that people might like
+#I "bin/net45"
+#I "../bin/net45"
+#I "../../bin/net45"
+#I "../../bin/BioFSharp.IO/net45"
+#I "../../../bin/net45"
+#I "lib"
 
+// Reference BioFSharp.IO
+#r "BioFSharp.IO.dll"
+
+open BioFSharp.IO
+
+do fsi.AddPrinter(FSIPrinters.prettyPrintBioCollection)
+do fsi.AddPrinter(FSIPrinters.prettyPrintBioItem)
+do fsi.AddPrinter(FSIPrinters.prettyPrintClustal)
+do fsi.AddPrinter(FSIPrinters.prettyPrintGFF3)
+do fsi.AddPrinter(FSIPrinters.prettyPrintGPL)
+do fsi.AddPrinter(FSIPrinters.prettyPrintGSE)
