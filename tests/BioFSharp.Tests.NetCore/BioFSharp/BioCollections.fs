c921a851e0279a39f7ef56c229c5511c33a19a19
Graeme Vissers <vissersgraeme@gmail.com>
2020-07-13 10:41:35 -0700

    Add toAverageMass unit test to BioArray testList

--- tests/BioFSharp.Tests.NetCore/BioFSharp/BioCollections.fs
+++ tests/BioFSharp.Tests.NetCore/BioFSharp/BioCollections.fs
@@ -149,6 +149,15 @@ let bioCollectionsTests  =
                     (131.04048 + 99.06841 + 113.08406)
                     "BioArray.toMonoisotopicMass did not return correct mass"
             )
+
+            testCase "toAverageMass" (fun() ->
+                Expect.floatClose
+                    Accuracy.medium // High accuracy was not passing test
+                    (testProt |> BioArray.toAverageMass)
+                    // Masses obtained from University of Washington Proteomics Resource https://proteomicsresource.washington.edu/protocols06/masses.php
+                    (131.19606 + 99.13106 + 113.15764)
+                    "BioArray.toAverageMass did not return correct mass"
+            )
         ]
 
         testList "BioList" [
