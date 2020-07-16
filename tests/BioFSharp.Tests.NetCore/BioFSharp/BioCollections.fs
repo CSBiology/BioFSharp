21dcc2b41004559fc7da374ff2936974d6aa55d3
Graeme Vissers <vissersgraeme@gmail.com>
2020-07-16 11:37:50 -0700

    Remove dependence on BioItem.symbol for test

--- tests/BioFSharp.Tests.NetCore/BioFSharp/BioCollections.fs
+++ tests/BioFSharp.Tests.NetCore/BioFSharp/BioCollections.fs
@@ -179,9 +179,9 @@ let bioCollectionsTests  =
 
             testCase "toCompositionVector" (fun () ->
                 let testCompVec = Array.zeroCreate 26
-                let metIndex = (int(BioItem.symbol Met)) - 65
-                let valIndex = (int(BioItem.symbol Val)) - 65
-                let leuIndex = (int(BioItem.symbol Leu)) - 65
+                let metIndex = 12 // Value of (int(BioItem.symbol Met)) - 65
+                let valIndex = 21 // Value of (int(BioItem.symbol Val)) - 65
+                let leuIndex = 11 // Value of (int(BioItem.symbol Leu)) - 65
                 testCompVec.[metIndex] <- testCompVec.[metIndex] + 1
                 testCompVec.[valIndex] <- testCompVec.[valIndex] + 1
                 testCompVec.[leuIndex] <- testCompVec.[leuIndex] + 1
