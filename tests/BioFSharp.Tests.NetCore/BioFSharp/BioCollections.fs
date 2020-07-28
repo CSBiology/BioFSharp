17c6edb0bb6f9c87de011c8617ec82a0aae213ba
Graeme Vissers <vissersgraeme@gmail.com>
2020-07-28 14:57:36 -0700

    Add testCase "toAverageMassWith"

--- tests/BioFSharp.Tests.NetCore/BioFSharp/BioCollections.fs
+++ tests/BioFSharp.Tests.NetCore/BioFSharp/BioCollections.fs
@@ -469,5 +469,14 @@ let bioCollectionsTests  =
                         (131.04048 + 99.06841 + 113.08406 + 18.0)
                         "BioSeq.toMonoisotopicMassWith did not return correct mass"
                 )
+
+                testCase "toAverageMassWith" (fun () ->
+                    Expect.floatClose
+                        Accuracy.medium
+                        (testProt |> Seq.ofArray |> BioSeq.toAverageMassWith 18.0) // 18 = mass of one water molecule
+                        // Masses obtained from University of Washington Proteomics Resource https://proteomicsresource.washington.edu/protocols06/masses.php
+                        (131.19606 + 99.13106 + 113.15764 + 18.0)
+                        "BioSeq.toAverageMassWith did not return correct mass"
+                )
         ]
     ]
