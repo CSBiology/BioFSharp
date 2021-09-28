aa5afea870d21b8808cf26dbf5e9234cd7286a93
Kevin Schneider <schneider.kev@outlook.de>
2021-09-28 15:55:59 +0200

    Fix test execution

--- tests/BioFSharp.Tests/Main.fs
+++ tests/BioFSharp.Tests/Main.fs
@@ -1,27 +1,6 @@
-﻿module BioFSharp.Tests.NetCore
-
+﻿module Plotly.NET.Tests
 open Expecto
 
 [<EntryPoint>]
 let main argv =
-
-    //BioFSharp core tests
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testIsotopes         |> ignore
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testElements         |> ignore
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testFormula          |> ignore
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testMass             |> ignore
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testIBioItem         |> ignore
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testTaggedSequence   |> ignore
-    Tests.runTestsWithCLIArgs [] argv BioItemTests.testIsotopicDistribution     |> ignore
-
-    Tests.runTestsWithCLIArgs [] argv BioCollectionsTests.bioCollectionsTests |> ignore
-
-    Tests.runTestsWithCLIArgs [] argv DigestionTests.digestionTests |> ignore
-    
-    Tests.runTestsWithCLIArgs [] argv AminoAcidTests.aminoAcidTests |> ignore
-
-    Tests.runTestsWithCLIArgs [] argv NucleotideTests.nucleotideTests |> ignore
-
-    Tests.runTestsWithCLIArgs [] argv PhylTreeTests.phylTreeTests |> ignore
-
-    0
+    Tests.runTestsInAssemblyWithCLIArgs [] argv
