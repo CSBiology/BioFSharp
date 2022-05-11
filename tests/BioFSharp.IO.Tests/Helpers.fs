8f86b6e7209f19099a33136cdb8ddfa4c4aca7ef
Kevin Schneider <schneider.kev@outlook.de>
2022-05-11 17:50:50 +0200

    try fix CI test fun via embedded resources

---
+++ tests/BioFSharp.IO.Tests/Helpers.fs
@@ -0,0 +1,14 @@
+﻿module Helpers
+
+open System
+open System.Reflection
+
+let readEmbeddedDocument resourceName= 
+    let assembly = Assembly.GetExecutingAssembly()
+    use stream = assembly.GetManifestResourceStream("BioFSharp.IO.Tests.data." + resourceName)
+    use reader = new System.IO.StreamReader(stream)
+    seq [
+        while not reader.EndOfStream do                              
+        yield reader.ReadLine() 
+    ]
+    |> Array.ofSeq
